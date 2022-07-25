6dbdf9e4a020dc03468ee703fb63871ce7f8e150
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2022-07-25 22:23:28 +0100

    updates - added models -> product , comment - added the scrappers

--- price_compare/models.py
+++ price_compare/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.utils.text import slugify
+from uuid import uuid4
+from django.urls import reverse
 
 # Create your models here.
+class Product(models.Model):
+    CATEGORY_CHOICES = (
+        ('smartphones', 'Smartphones'),
+        ('laptops','Laptops'),
+        ('desktop-computers','Desktop-computers'),
+        ('television','Television'),
+        ('smart-tvs','Smart-tvs'),
+        ('digital-cameras','Digital-cameras'),
+        ('generators','Generators')
+    )
+    id = models.UUIDField(default=uuid4(),unique=True,primary_key=True)
+    name = models.CharField(max_length=614)
+    brand = models.CharField(max_length=256)
+    img_src = models.CharField(max_length=614)
+    slug = models.SlugField(blank=True,max_length=124)
+    category = models.CharField(max_length=32,choices=CATEGORY_CHOICES)
+
+    def save(self,*args,**kwargs):
+        self.slug = slugify(self.name)
+        super(Product,self).save(*args,**kwargs)
+
+    def get_absolute_url(self):
+        #product_detail is the view
+        return reverse("price_compare:product_detail", args=[
+            self.id,
+            self.slug
+        ])
+    
+
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='comments')
+    username = models.CharField(max_length=614)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Comment by {self.username} on {self.product}'
+
--- price_compare/views.py
+++ price_compare/views.py
@@ -1,7 +1,54 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import CommentForm
+from django.views.generic import ListView
+from django.shortcuts import get_object_or_404, render
+from .models import Product 
+from .scraper.jumia import get_jumia_product
 
 # Create your views here.
 
-def test(request):
-   return HttpResponse('hello, world!')
+def product_detail(request,id,product):
+    product = get_object_or_404(
+        Product,
+        slug=product,
+        id = id,
+    )
+
+    comments = product.comments.filter(active=True)
+    new_comment = None
+    platforms = []
+    prd = {
+        'name':product.name,
+        'brand':product.brand,
+        'category':product.category
+    }
+    platforms.append(get_jumia_product(prd))
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.product = product
+            new_comment.save()
+    else:
+            comment_form = CommentForm()
+
+    return render(request,'price_compare/product/detail.html',
+                    {
+                        'product':product,
+                        'comments':comments,
+                        'platforms':platforms,
+                        'new_comment':new_comment,
+                        'comment_form':comment_form
+                    },            
+    )
+
+
+class ProductListView(ListView):
+    model = Product
+    context_object_name = 'products'
+    template_name = 'price_compare/product/list.html'
