fac987a805f0bc83c5e288e7645e204be0ed4a69
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2022-07-27 16:29:06 +0100

    added postgreql

--- team23/settings.py
+++ team23/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_social_share',
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
@@ -84,8 +85,19 @@ WSGI_APPLICATION = 'team23.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': '<db_name>',
+
+        'USER': '<username>',
+
+        'PASSWORD': '<password>',
+
+        'HOST': '<host>',
+
+        'PORT': '<port>',
+
     }
 }
 
